cb34d47f8605804d9eb8267b57ae7e35c99e38c2
123qwertyuiopaz <thejeshreddy2007@outlook.com>
2026-02-27 16:13:12 +0530

    UC1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,28 +3,9 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        Stack<Character> stack = new Stack<>();
 
-        System.out.println("=== Stack-Based Palindrome Checker ===");
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
 
-        String processedInput = input.replaceAll("\\s+", "").toLowerCase();
+        System.out.println("welcome to the palindrome checker mangement system");
 
-        for (int i = 0; i < processedInput.length(); i++) {
-            stack.push(processedInput.charAt(i));
-        }
-        String reversedString = "";
-        while (!stack.isEmpty()) {
-            reversedString += stack.pop();
-        }
-        if (processedInput.equals(reversedString)) {
-            System.out.println("Result: The given string is a Palindrome.");
-        } else {
-            System.out.println("Result: The given string is NOT a Palindrome.");
-        }
-
-        scanner.close();
     }
 }
