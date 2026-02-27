770c33b699808912c81d6494df799ebcd3247d96
123qwertyuiopaz <thejeshreddy2007@outlook.com>
2026-02-27 16:15:55 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,14 @@
 
 public class PalindromeCheckerApp {
+//UseCase2
 
     public static void main(String[] args) {
-
-
-
-        System.out.println("welcome to the palindrome checker mangement system");
-
+        String word = "madam";
+        String reversedWord = new StringBuilder(word).reverse().toString();
+        if (word.equals(reversedWord)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
     }
 }
