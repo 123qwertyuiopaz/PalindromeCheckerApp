b3727dbb0db8c3017819aaa6dad69cd754b3c34b
123qwertyuiopaz <thejeshreddy2007@outlook.com>
2026-02-27 14:52:27 +0530

    use case 2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+
 class PalindromeCheckerApp {
+
+
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("welcome to palindrome checker mangement system");//use case one
+=======
+        Scanner sc = new Scanner(System.in);
+        System.out.println("welcome to palindrome checker mangement system");//use case 1
+
+        System.out.println("Enter words to check ");//use case 1
+            String word = sc.nextLine();
+            String reversed = new StringBuilder(word).reverse().toString();
+            if (word.equals(reversed)) {
+                System.out.println(word + " is a palindrome.");
+            } else {
+                System.out.println(word + " is not a palindrome.");
+            }
+>>>>>>> 98075cc (UC1: Add welcome message module)
     }
 }
