d1627ad6dc156da4aac416e22b84b63d9940ff80
123qwertyuiopaz <thejeshreddy2007@outlook.com>
2026-03-09 15:39:05 +0530

    UC9: Add palindrome validation logic

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,42 +1,34 @@
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Scanner;
-//UseCase7
+
 public class PalindromeCheckerApp {
 
-
-    public static boolean isPalindrome(String input) {
-
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
-
-        Deque<Character> deque = new LinkedList<>();
-        for (char ch : normalized.toCharArray()) {
-            deque.addLast(ch);
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
         }
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-            if (front != rear) {
-                return false;
-            }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
-        return true;
+        return isPalindrome(str, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== UC7: Deque-Based Palindrome Checker ===");
+        System.out.println("=== UC9: Recursive Palindrome Checker App ===");
         System.out.print("Enter a string to check: ");
         String input = scanner.nextLine();
 
-        if (isPalindrome(input)) {
-            System.out.println("\"" + input + "\" is a Palindrome.");
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean result = isPalindrome(normalized, 0, normalized.length() - 1);
+
+        if (result) {
+            System.out.println("\"" + input + "\" is a palindrome.");
         } else {
-            System.out.println("\"" + input + "\" is NOT a Palindrome.");
+            System.out.println("\"" + input + "\" is NOT a palindrome.");
         }
 
         scanner.close();
     }
 }
-
