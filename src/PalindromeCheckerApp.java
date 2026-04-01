8912720d51d55ab1950c822f04f5cf8974aadadd
123qwertyuiopaz <thejeshreddy2007@outlook.com>
2026-04-01 15:35:33 +0530

    UC5

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,21 +1,22 @@
+import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String original = "level";
-        char[] charArray = original.toCharArray();
-        int start = 0;
-        int end = charArray.length - 1;
-        boolean isPalindrome = true;
-        while (start < end) {
-            if (charArray[start] != charArray[end]) {
-                isPalindrome = false;
-            }
-            start++;
-            end--;
+        String original = "radar";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < original.length(); i++) {
+            stack.push(original.charAt(i));
         }
-        if (isPalindrome) {
-            System.out.println("'" + original + "' is a palindrome.");
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a palindrome.");
         } else {
-            System.out.println("'" + original + "' is NOT a palindrome.");
+            System.out.println(original + " is NOT a palindrome.");
         }
     }
 }
